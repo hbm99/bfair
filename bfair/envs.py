933e406530b88ca974e3dd99ff51301532769231
Hansel Blanco <ernestoblancomalmierca@gmail.com>
2023-05-18 18:05:33 -0400

    Update `UTK_FACE_DATASET` address.

--- bfair/envs.py
+++ bfair/envs.py
@@ -2,6 +2,6 @@ ADULT_DATASET = "datasets/adult/"
 COMPAS_DATASET = "datasets/compas/"
 GERMAN_DATASET = "datasets/german/"
 IMDB_REVIEWS_DATASET = "datasets/imdb-reviews/"
-UTK_FACE_DATASET = "/Users/hanselblanco/Documents/4to/ML/UTKFace/UTKFace/"
+UTK_FACE_DATASET = "datasets/utkface/"
 
 DEBIASED_WORD2VEC_PATH = "embeddings/GoogleNews-vectors-negative300-hard-debiased.bin"
