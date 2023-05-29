62700560fed7c8ec5aa69b5a4a24376668bc3aa1
Hansel Blanco <ernestoblancomalmierca@gmail.com>
2023-05-29 13:32:15 -0400

    Add `filter`.

--- bfair/sensors/image/clip/base.py
+++ bfair/sensors/image/clip/base.py
@@ -1,5 +1,5 @@
 
-from typing import List, Set, Union
+from typing import List, Set
 
 import clip
 import numpy as np
@@ -7,16 +7,18 @@ import torch
 from autogoal.kb import Matrix, SemanticType
 
 from bfair.sensors.base import Sensor
+from bfair.sensors.text.embedding.filters import Filter
 
-BATCH_SIZE = 1000
+BATCH_SIZE = 64
 
 
 class ClipBasedSensor(Sensor):
     
-    def __init__(self, restricted_to: str | Set[str] = None) -> None:
+    def __init__(self, filter: Filter, restricted_to: str | Set[str] = None) -> None:
         super().__init__(restricted_to)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model, self.preprocess = clip.load("ViT-B/32", self.device)
+        self.filter = filter
     
     
     def __call__(self, item, attributes: List[str], attr_cls: str):
@@ -33,19 +35,22 @@ class ClipBasedSensor(Sensor):
         
         results = []
         for i in range(0, len(item), min(BATCH_SIZE, len(item) - i)):
-            images = [self.preprocess(photo) for photo in range(i, min(i + BATCH_SIZE, len(item)))]
+            images = [self.preprocess(item[photo_idx]) for photo_idx in range(i, min(i + BATCH_SIZE, len(item)))]
             image_input = torch.tensor(np.stack(images)).to(self.device)
             with torch.no_grad():
                 logits_per_image, _ = self.model(image_input, text)
-                probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-                results.append(probs)
+                
+                batch_probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+                attribute_probs = [(attributes[j], image_probs[j]) for j in range(len(attributes)) for image_probs in batch_probs]
+                attributed_tokens = [(str(images[j]), attribute_probs[j]) for j in range(len(images))] 
+                
+                results.append(attributed_tokens)
         
-        choices = np.argmax(np.concatenate(results, axis=0), axis=1)
+        flatten_results = np.concatenate(results, axis=0)
         
-        get_label = lambda x: attributes[x]
-        v_get_label = np.vectorize(get_label)
+        attributed_tokens = self.filter(flatten_results)
         
-        return v_get_label(choices)
+        return attributed_tokens # pending tests, waiting for docker compose to be fixed
 
     def _get_input_type(self) -> SemanticType:
         return Matrix
