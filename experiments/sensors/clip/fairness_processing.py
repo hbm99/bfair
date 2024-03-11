ff00af3954569d5ffb7a293565ec8907429f5bdf
Hansel Blanco <ernestoblancomalmierca@gmail.com>
2024-03-11 12:01:27 -0400

    Refactor `get_SP` function to include sense calculation.

--- experiments/sensors/clip/fairness_processing.py
+++ experiments/sensors/clip/fairness_processing.py
@@ -2,15 +2,26 @@ from statistics import mean
 from typing import Dict
 
 
-def get_SP(true_prs: Dict[str, float], est_prs: Dict[str, float], macro: bool = True):
+def get_SP_sense(true_prs: Dict[str, float], est_prs: Dict[str, float]):
+    true_prs = {k.lower(): v for k, v in true_prs.items()}
+
     true_diffs = get_diffs(true_prs)
     est_diffs = get_diffs(est_prs)
 
     diffs = {}
     for k in true_diffs.keys():
-        diffs[k] = abs(true_diffs[k] - est_diffs[k])
+        diffs[k] = abs(abs(true_diffs[k]) - abs(est_diffs[k]))
 
-    return diffs if not macro else mean(diffs.values())
+    senses = {}
+    for k in true_diffs.keys():
+        senses[k] = 1 if true_diffs[k] * est_diffs[k] > 0 else 0
+
+    return (
+        diffs,
+        mean(diffs.values()),
+        senses,
+        mean(senses.values()),
+    )
 
 
 def get_diffs(prs):
@@ -22,36 +33,41 @@ def get_diffs(prs):
             k = str({k_1, k_2})
             if k in diffs.keys():
                 continue
-            diffs[k] = abs(v_1 - v_2)
+            diffs[k] = v_1 - v_2
     return diffs
-
-
-def get_sense(true_prs, est_prs):
-    pass
 
 
 true_f = (
     0.07333482843686928,
     {
-        "black": 0.4719626168224299,
-        "east asian": 0.49391727493917276,
-        "indian": 0.4838709677419355,
-        "latino_hispanic": 0.5117370892018779,
-        "middle eastern": 0.44314868804664725,
-        "southeast asian": 0.5164835164835165,
-        "white": 0.48312611012433393,
+        "Black": 0.4719626168224299,
+        "East Asian": 0.49391727493917276,
+        "Indian": 0.4838709677419355,
+        "Latino_Hispanic": 0.5117370892018779,
+        "Middle Eastern": 0.44314868804664725,
+        "Southeast Asian": 0.5164835164835165,
+        "White": 0.48312611012433393,
     },
 )
 est_f = (
-    0.09960468058191019,
+    0.11025641025641025,
     {
-        "black": 0.47146401985111663,
-        "east asian": 0.5318627450980392,
-        "indian": 0.49127906976744184,
-        "latino_hispanic": 0.528169014084507,
-        "middle eastern": 0.432258064516129,
-        "southeast asian": 0.49666666666666665,
-        "white": 0.49557522123893805,
+        "black": 0.4666666666666667,
+        "east asian": 0.5256410256410257,
+        "indian": 0.49206349206349204,
+        "latino_hispanic": 0.5108695652173914,
+        "middle eastern": 0.4153846153846154,
+        "southeast asian": 0.5038461538461538,
+        "white": 0.5013404825737265,
     },
 )
-print(get_SP(true_f[1], est_f[1]))
+sp, sp_mean, sense, sense_mean = get_SP_sense(true_f[1], est_f[1])
+
+print("SP")
+print(sp)
+print("SP mean")
+print(sp_mean)
+print("sense")
+print(sense)
+print("sense mean")
+print(sense_mean)
